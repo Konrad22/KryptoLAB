881aa280aea1dc15387c3d5b4bbc29d5bac5b373
Konrad Schwede <ge76vul@ppc722.mirz.uni-jena.de>
2024-03-11 13:10:11 +0100

    comments for U3

--- U3_Betriebsmodi/functions.py
+++ U3_Betriebsmodi/functions.py
@@ -1,8 +1,10 @@
 import math as m
 
+#adds a string made up of '0' of the lenght of x to a string <message>
 def add_x_bits(message, x:int):
     return message + x*'0'
 
+#partitions a string <message> into blocks of lenght <t>
 def t_block_partition(message, t:int):
     l = len(message)
     a = m.ceil(l/t)
@@ -18,30 +20,31 @@ def t_block_partition(message, t:int):
         partitions.append(message[(i - 1)*t, i*t - 1])
     return partitions
 
+#partitions a string <message> into blocks of lenght 64
 def block_partition_sixtyfour(message):
     return t_block_partition(message, 64)
 
-def identity_encryption(message):
-    return message
-
-def ECB_encrypt(plain, t:int, encryption):             
-    partitions = t_block_partition(plain, t)
+#encrypts a bitstring using the ECB block cypher mode of operation, with blocklenght being a variable <t>
+def ECB_encrypt(plain_text, t:int, encryption):             
+    partitions = t_block_partition(plain_text, t)
     encrypted_partitions = encryption(partitions)
     encrypted_text = ''
     for partition in encrypted_partitions:
         encrypted_text =+ partition
     return encrypted_text
 
-def ECB_decrypt(cypher, t:int, decryption):             
-    partitions = t_block_partition(cypher, t)
+#decrypts a bitstring using the ECB block cypher mode of operation, with blocklenght being a variable <t>
+def ECB_decrypt(cypher_text, t:int, decryption):             
+    partitions = t_block_partition(cypher_text, t)
     decrypted_partitions = decryption(partitions)
     decrypted_text = ''
     for partition in decrypted_partitions:
         decrypted_text =+ partition
     return decrypted_text
 
-def CBC_encrypt(plain, t, encryption, vector):
-    partitions = t_block_partition(plain, t)
+#encrypts a bitstring using the CBC block cypher mode of operation, with blocklenght being a variable <t>
+def CBC_encrypt(plain_text, t, encryption, vector):
+    partitions = t_block_partition(plain_text, t)
     partitions[0] = encryption(partitions[0] ^ vector)
     for i in range(len(partitions)):
         partitions[i+1] = encryption(partitions[i] ^ partitions[i+1])
@@ -50,8 +53,9 @@ def CBC_encrypt(plain, t, encryption, vector):
         encrypted_text =+ partition
     return encrypted_text
 
-def CBC_decrypt(cypher, t, decryption, vector):          
-    partitions = t_block_partition(cypher, t)
+#decrypts a bitstring using the CBC block cypher mode of operation, with blocklenght being a variable <t>
+def CBC_decrypt(cypher_text, t, decryption, vector):          
+    partitions = t_block_partition(cypher_text, t)
     j = len(partitions)
     for i in range(j):
         partitions[j-i] = decryption(partitions[j-i]) ^ partitions[j-i-1]
@@ -60,8 +64,9 @@ def CBC_decrypt(cypher, t, decryption, vector):
         decrypted_text =+ partition
     return decrypted_text
 
-def OFB_encrypt(plain, t, encryption, vector):
-    partitions = t_block_partition(plain, t)
+#encrypts a bitstring using the OFB block cypher mode of operation, with blocklenght being a variable <t>
+def OFB_encrypt(plain_text, t, encryption, vector):
+    partitions = t_block_partition(plain_text, t)
     encrypted_vector = encryption(vector)
     for i in range(len(partitions)):
         partitions[i] = partitions[i] ^ encrypted_vector
@@ -69,9 +74,10 @@ def OFB_encrypt(plain, t, encryption, vector):
     for partition in partitions:
         encrypted_text =+ partition
     return encrypted_text
-    
-def OFB_decrypt(cypher, t, encryption, vector):
-    partitions = t_block_partition(cypher, t)
+
+#decrypts a bitstring using the OFB block cypher mode of operation, with blocklenght being a variable <t>
+def OFB_decrypt(cypher_text, t, encryption, vector):
+    partitions = t_block_partition(cypher_text, t)
     encrypted_vector = encryption(vector)
     for i in range(len(partitions)):
         partitions[i] = partitions[i] ^ encrypted_vector
@@ -80,16 +86,18 @@ def OFB_decrypt(cypher, t, encryption, vector):
         decrypted_text =+ partition
     return decrypted_text
 
-def CTR_encrypt(plain, t, encryption):
-    partitions = t_block_partition(plain, t)
+#encrypts a bitstring using the CTR block cypher mode of operation, with blocklenght being a variable <t>
+def CTR_encrypt(plain_text, t, encryption):
+    partitions = t_block_partition(plain_text, t)
     encrypted_text = ''
     for i in range(len(partitions)):
         j = i % 2**t
         encrypted_text =+ encryption(j) ^ partitions[j]
     return encrypted_text
 
-def CTR_decrypt(cypher, t, encryption):
-    partitions = t_block_partition(cypher, t)
+#decrypts a bitstring using the CTR block cypher mode of operation, with blocklenght being a variable <t>
+def CTR_decrypt(cypher_text, t, encryption):
+    partitions = t_block_partition(cypher_text, t)
     decrypted_text = ''
     for i in range(len(partitions)):
         j = i % 2**t
